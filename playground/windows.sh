e9f355b2eb3a64dd68b2e6bfa3a4c56d48f36b18
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-08-19 12:40:04 +0200

    Update windows.sh

--- playground/windows.sh
+++ playground/windows.sh
@@ -13,21 +13,21 @@ az network vnet create --address-prefixes 10.$nwindex.0.0/16 --name $rg-vnet --r
 az network vnet subnet create -g $rg --vnet-name $rg-vnet -n AzureBastionSubnet \
     --address-prefixes 10.$nwindex.1.0/24 #--network-security-group $rg-Nsg --route-table {$rg}RouteTable
 
-az network public-ip create -g $rg -n Bastion-PIP --sku Standard
+az network public-ip create -g $rg -n $rg-Bastion-PIP --sku Standard
     
 az network bastion create --location $location --name $rg-bastionhost --public-ip-address $rg-Bastion-PIP --resource-group $rg --vnet-name $rg-vnet
 
 az network public-ip create -g $rg -n AzureMigratPhysicalAppliance-PIP --sku Standard
 
-az vm create --name $vmname \
-    --resource-group $rg \
-    --computer-name $computername \
+az vm create --name {$vmname} \
+    --resource-group {$rg} \
+    --computer-name {$computername} \
     --image win2016datacenter  \
     --authentication-type password \
-    --admin-username $adminusername \
-    --admin-password $adminpassword \
+    --admin-username {$adminusername} \
+    --admin-password {$adminpassword} \
     --license-type Windows_Server \
-    --vnet-name $rg-vnet \
+    --vnet-name {$rg}-vnet \
     --subnet default \
-    --public-ip-address $vmname-PIP \
+    --public-ip-address {$vmname}-PIP \
     --size Standard_D2s_v3 
