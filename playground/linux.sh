a951c42ddc4381f54fb1d775cd01958f3f2523bc
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-08-20 12:35:01 +0200

    Update linux.sh

--- playground/linux.sh
+++ playground/linux.sh
@@ -8,9 +8,30 @@ cd -- $DIR
 
 source linux.cfg
 
+az group create --name $rg --location $location
+
+az vm image list --output table
+az vm image list --offer UbuntuServer --all --output table
+az vm list-sizes --location $location --output table
+
 az vm create \
     --resource-group myResourceGroupVM \
-    --name myVM \
+    --name $vmname \
     --image $vmimage \
-    --admin-username azureuser \
-    --generate-ssh-keys
+    --admin-username $adminusername \
+    --generate-ssh-keys #  --data-disk-sizes-gb 128 128
+
+#Management
+az vm list-ip-addresses --resource-group $rg --name $vmname --output table
+    
+#Resize a VM
+
+#az vm list-sizes --location $location --output table
+#az vm show --resource-group $rg --name $vmname --query hardwareProfile.vmSize
+#az vm list-vm-resize-options --resource-group $rg --name $vmname --query [].name
+#az vm resize --resource-group $rg --name $vmname --size Standard_DS4_v2
+#az vm deallocate --resource-group $rg --name $vmname
+#az vm resize --resource-group $rg --name $vmname --size Standard_GS1
+#az vm start --resource-group $rg --name $vmname
+
+#nwindex=10
