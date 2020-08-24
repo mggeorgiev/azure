7ef100fea1110bc5735d184c65f83f742556c9d0
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-08-24 16:06:35 +0200

    Create resource_group.tf

---
+++ playground/terraform/resource_group.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" playgroundgroup {
+    name     = [var.resource_group]
+    location = [var.location]
+
+    tags = {
+        environment = var.environementtag,
+        billing-code = var.billing-code
+    }
+}
