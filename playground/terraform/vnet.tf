58996ce0f1cecabc49ecf8cbea70cb550890937b
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-08-24 16:27:29 +0200

    Update vnet.tf

--- playground/terraform/vnet.tf
+++ playground/terraform/vnet.tf
@@ -1,6 +1,6 @@
 # Create virtual network
 resource "azurerm_virtual_network" "playgroundnetwork" {
-    name                = "$var.resource_group-vnet"
+    name                = "${var.resource_group}-vnet"
     address_space       = ["10.10.0.0/16"]
     location            = var.location
     resource_group_name = azurerm_resource_group.playgroundgroup.name
