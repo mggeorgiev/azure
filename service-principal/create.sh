159650bba23948f952b1c4c5c395843f1b0e2616
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-08-26 12:27:35 +0200

    Update create.sh

--- service-principal/create.sh
+++ service-principal/create.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+#consult https://medium.com/@gmusumeci/deploying-terraform-infrastructure-using-azure-devops-pipelines-step-by-step-advanced-1281b4ee15d1
 
 az login
 az account list --output table
 
 az account set --subsctiption $subscription
 
-az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$subscription" --name="Azure-DevOps" 
+az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$subscription" --name="Azure-DevOps"
+
+#appId (Azure) → client_id (Terraform).
+#password (Azure) → client_secret (Terraform).
+#tenant (Azure) → tenant_id (Terraform).
+
+#https://marketplace.visualstudio.com/items?itemName=charleszipp.azure-pipelines-tasks-terraform
