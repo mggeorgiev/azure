026144243c0b7f6d8f5992e7e8a46eee402ec056
“Martin <mggeorgiev@gmail.com>
2020-08-30 08:40:58 +0200

    move cli files to a dedicated directory

--- playground/terraform/resource_group.tf
+++ playground/terraform/resource_group.tf
@@ -1,6 +1,7 @@
 resource "azurerm_resource_group" playgroundgroup {
     name     = var.resource_group
     location = var.location
+    subscription_id = var.subscriptionID
 
     tags = {
         environment = var.environementtag,
