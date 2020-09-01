1fdda60c7cf28e86c9bf676949f5302bb565b09d
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-09-01 16:52:51 +0200

    Update linuxvm.tf

--- playground/terraform/linuxvm.tf
+++ playground/terraform/linuxvm.tf
@@ -1,9 +1,9 @@
 # Create public IPs
 resource "azurerm_public_ip" "linuxpublicip" {
-    name                         = "${var.vmnamelinux}VM-PIP"
-    location                     = var.location
-    resource_group_name          = azurerm_resource_group.playgroundgroup.name
-    allocation_method            = "Dynamic"
+    name                                = "${var.vmnamelinux}VM-PIP"
+    location                            = var.location
+    resource_group_name                 = azurerm_resource_group.playgroundgroup.name
+    allocation_method                   = "Dynamic"
 
     tags = {
         environment = var.environementtag,
@@ -13,15 +13,15 @@ resource "azurerm_public_ip" "linuxpublicip" {
 
 # Create network interface
 resource "azurerm_network_interface" "linuxvmnic" {
-    name                      = "${var.vmnamelinux}VM-NIC"
-    location                  = var.location
-    resource_group_name       = azurerm_resource_group.playgroundgroup.name
+    name                                = "${var.vmnamelinux}VM-NIC"
+    location                            = var.location
+    resource_group_name                 = azurerm_resource_group.playgroundgroup.name
 
     ip_configuration {
-        name                          = "${var.vmnamelinux}NicConfiguration"
-        subnet_id                     = azurerm_subnet.playgroundsubnet.id
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = azurerm_public_ip.linuxpublicip.id
+        name                            = "${var.vmnamelinux}NicConfiguration"
+        subnet_id                       = azurerm_subnet.playgroundsubnet.id
+        private_ip_address_allocation   = "Dynamic"
+        public_ip_address_id            = azurerm_public_ip.linuxpublicip.id
     }
 
     tags = {
@@ -32,8 +32,8 @@ resource "azurerm_network_interface" "linuxvmnic" {
 
 # Connect the security group to the network interface
 resource "azurerm_network_interface_security_group_association" "example" {
-    network_interface_id      = azurerm_network_interface.linuxvmnic.id
-    network_security_group_id = azurerm_network_security_group.playgroundnsg.id
+    network_interface_id                = azurerm_network_interface.linuxvmnic.id
+    network_security_group_id           = azurerm_network_security_group.playgroundnsg.id
 }
 
 # Create (and display) an SSH key
@@ -45,16 +45,16 @@ output "tls_private_key" { value = "${tls_private_key.example_ssh.private_key_pe
 
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "playgroundlinuxvm" {
-    name                  = var.vmnamelinux
-    location              = var.location
-    resource_group_name   = azurerm_resource_group.playgroundgroup.name
-    network_interface_ids = [azurerm_network_interface.linuxvmnic.id]
-    size                  = "Standard_DS1_v2"
+    name                                = var.vmnamelinux
+    location                            = var.location
+    resource_group_name                 = azurerm_resource_group.playgroundgroup.name
+    network_interface_ids               = [azurerm_network_interface.linuxvmnic.id]
+    size                                = "Standard_DS1_v2"
 
     os_disk {
-        name              = "${var.vmnamelinux}OsDisk"
-        caching           = "ReadWrite"
-        storage_account_type = "Premium_LRS"
+        name                            = "${var.vmnamelinux}OsDisk"
+        caching                         = "ReadWrite"
+        storage_account_type            = "Premium_LRS"
     }
 
     source_image_reference {
@@ -64,17 +64,17 @@ resource "azurerm_linux_virtual_machine" "playgroundlinuxvm" {
         version   = "latest"
     }
 
-    computer_name  = "${var.vmnamelinux}SandBox"
-    admin_username = var.users[1]
-    disable_password_authentication = true
+    computer_name                       = "${var.vmnamelinux}SandBox"
+    admin_username                      = var.users[1]
+    disable_password_authentication     = true
         
     admin_ssh_key {
-        username       = var.users[1]
-        public_key     = tls_private_key.example_ssh.public_key_openssh
+        username                        = var.users[1]
+        public_key                      = tls_private_key.example_ssh.public_key_openssh
     }
 
     boot_diagnostics {
-        storage_account_uri = azurerm_storage_account.playgroundstorageaccount.primary_blob_endpoint
+        storage_account_uri             = azurerm_storage_account.playgroundstorageaccount.primary_blob_endpoint
     }
 
     tags = {
