b1d50ada463f9c3b814c58fd571e3d2a58a62c73
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-09-08 12:08:30 +0200

    Update main.tf

--- backup/terraform/main.tf
+++ backup/terraform/main.tf
@@ -1,1 +1,12 @@
+resource "azurerm_recovery_services_vault" "rsvault" {
+  name                = var.rsvault
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rgoadglobal.name
+  sku                 = "Standard"
 
+  soft_delete_enabled = true
+
+  tags = {
+        reviewbefore = formatdate("YYYY-MMM-DD",timeadd(timestamp(), "9m"))
+    }
+}
