39839517102842244de3542d5f05268e1aa8f196
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-09-08 11:03:38 +0200

    Update main.tf

--- network/terraform/main.tf
+++ network/terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
       resource_group_name   = "tstate"
-      storage_account_name  = "tstate7824"
+      storage_account_name  = "tstate5640"
       container_name        = "tstate"
       key                   = "terraform.tfstate"
   }
@@ -25,14 +25,14 @@ resource "azurerm_resource_group" "rghubspoke" {
 
 # Create hub virtual network
 resource "azurerm_virtual_network" "vnetHub" {
-    name                  = var.vnetHub["name"]
-    address_space         = ["${var.vnetHub["address_space"]}"]
-    location              = azurerm_resource_group.rghubspoke.location
-    resource_group_name   = azurerm_resource_group.rghubspoke.name
+    name                = var.vnetHub["name"]
+    address_space       = ["${var.vnetHub["address_space"]}"]
+    location            = azurerm_resource_group.rghubspoke.location
+    resource_group_name = azurerm_resource_group.rghubspoke.name
 
     tags = {
-        environment       = var.vnetHub["environment"],
-        billing-code      = var.vnetHub["billing-code"]
+        environment     = var.vnetHub["environment"],
+        billing-code    = var.vnetHub["billing-code"]
     }
 }
 
@@ -110,16 +110,31 @@ resource "azurerm_subnet" "spoke2Subnet" {
     address_prefixes     = ["${var.vnetSpoke2["defaultSubnet"]}"]
 }
 
-resource "azurerm_virtual_network_peering" "hub2spoke1" {
-  name                      = "hub2spoke1"
+#Peerings
+resource "azurerm_virtual_network_peering" "hubtospoke1" {
+  name                      = "hubtospoke1"
   resource_group_name       = azurerm_resource_group.rghubspoke.name
   virtual_network_name      = azurerm_virtual_network.vnetHub.name
   remote_virtual_network_id = azurerm_virtual_network.vnetSpoke1.id
 }
 
-resource "azurerm_virtual_network_peering" "hub2spoke2" {
-  name                      = "hub2spoke2"
+resource "azurerm_virtual_network_peering" "spoke1tohub" {
+  name                      = "spoke1tohub"
+  resource_group_name       = azurerm_resource_group.rghubspoke.name
+  virtual_network_name      = azurerm_virtual_network.vnetSpoke1.name
+  remote_virtual_network_id = azurerm_virtual_network.vnetHub.id
+}
+
+resource "azurerm_virtual_network_peering" "hubtospoke2" {
+  name                      = "hubtospoke2"
   resource_group_name       = azurerm_resource_group.rghubspoke.name
   virtual_network_name      = azurerm_virtual_network.vnetHub.name
   remote_virtual_network_id = azurerm_virtual_network.vnetSpoke2.id
+}
+
+resource "azurerm_virtual_network_peering" "spoke2tohub" {
+  name                      = "spoke2tohub"
+  resource_group_name       = azurerm_resource_group.rghubspoke.name
+  virtual_network_name      = azurerm_virtual_network.vnetSpoke2.name
+  remote_virtual_network_id = azurerm_virtual_network.vnetHub.id
 }
