dd1c9b9d55fa8377e301c8fbe8428d70e8451a28
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-09-15 16:05:22 +0200

    Update main.tf

--- network/terraform/main.tf
+++ network/terraform/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "azurerm" {
-      resource_group_name   = "tstate"
-      storage_account_name  = "tstate5640"
-      container_name        = "tstate"
-      key                   = "terraform.tfstate"
-  }
-}
-
 provider "azurerm" {
   version = "~>2.0"
   features {}
