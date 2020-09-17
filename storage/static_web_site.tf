75d9078c191831e41a3310eaf182048ca88815da
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-09-17 15:41:04 +0200

    Update static_web_site.tf

--- storage/static_web_site.tf
+++ storage/static_web_site.tf
@@ -8,7 +8,8 @@ resource "azurerm_storage_account" "martingeorgievdata001" {
   resource_group_name       = azurerm_resource_group.martingeorgiev-staging-rg.name
   location                  = azurerm_resource_group.martingeorgiev-staging-rg.location
   account_tier              = "Standard"
-  account_replication_type  = "GRS"
+  account_replication_type  = "LRS"
+  account_kind              = "StorageV2"
 
   tags = {
     environment             = "staging"
