24238aff3756c6e32e50cdd96178f044f702f99e
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-09-27 22:48:02 +0200

    Update mail.tf

--- sql/mail.tf
+++ sql/mail.tf
@@ -1,27 +1,28 @@
-resource "azurerm_storage_account" "example" {
-  name                                    = "examplesa"
+resource "azurerm_storage_account" "stomssql" {
+  name                                    = "stomssql"
   resource_group_name                     = azurerm_resource_group.example.name
   location                                = azurerm_resource_group.example.location
   account_tier                            = "Standard"
   account_replication_type                = "LRS"
 }
 
-resource "azurerm_sql_server" "example" {
+resource "azurerm_sql_server" "mssqlserver" {
   name                                    = "mssqlserver"
   resource_group_name                     = azurerm_resource_group.example.name
   location                                = azurerm_resource_group.example.location
   version                                 = "12.0"
-  administrator_login                     = "mradministrator"
-  administrator_login_password            = "thisIsDog11"
+  administrator_login                     = "sqluser"
+  administrator_login_password            = "KBUBXF68vIAJVgBmpR2P"
 
   extended_auditing_policy {
-    storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
-    storage_account_access_key              = azurerm_storage_account.example.primary_access_key
+    storage_endpoint                        = azurerm_storage_account.stomssql.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.stomssql.primary_access_key
     storage_account_access_key_is_secondary = true
     retention_in_days                       = 6
   }
 
   tags = {
-    environment = "production"
+    environment   = "test",
+    billing-code  = "010"
   }
 }
