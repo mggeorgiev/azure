690b9b9e639f2d2171747112ead2332384c4afa4
Martin Georgiev <mggeorgiev@gmail.com>
2020-10-20 11:39:37 +0200

    update tg module

--- resource_group/terraform/main.tf
+++ resource_group/terraform/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" resourcegroup {
     name     = var.resource_group
-    location = var.location
+    location = var.rg_location
     subscription_id = var.subscriptionID
 
     tags = {
