220ad35981d6b356bf31c68b2617aa54337ce60a
Martin Georgiev <mggeorgiev@gmail.com>
2020-10-22 21:30:49 +0200

    create storage module

--- storage/storage-account-module/variables.tf
+++ storage/storage-account-module/variables.tf
@@ -1,23 +1,29 @@
 variable sto_location {
     type    = string
-    default = "eastus"
 }
 
 variable sto_name {
     type    = string
 }
 
+variable tier {
+    type        = string
+    description = "account_tier"
+}
+
+variable replication_type {
+    type        = string
+    description = "account_replication_type"
+}
+
 variable resource_group {
     type    = string
-    default = "rg-storage-01"
 }
 
 variable environementtag {
     type    = string
-    default = "production"
 }
 
 variable billing-code {
     type    = string
-    default = "001"
 }
