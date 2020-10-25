6b553c4440bb35a8453b9870788ec0c22412c42c
Martin Georgiev <mggeorgiev@gmail.com>
2020-10-25 18:12:49 +0100

    linuxvm initial version

---
+++ compute/vm/terraform/linux-vm-module/variables.tf
@@ -0,0 +1,75 @@
+# variables.tf
+
+variable "subscriptionID" {
+    type = string
+}
+
+variable "resource_group" {
+    type = string
+    default = "playground"
+}
+
+variable "location" {
+    type = string
+    default = "eastus"
+}
+
+variable "zone" {
+    type = string
+    default = 1
+}
+
+variable "vnet_subnet_id" {
+    type = string
+    desctiption = "Target subnet id"
+}
+
+variable "environementtag" {
+    type = string
+    default = "playground"
+}
+
+variable "billing-code" {
+    type = string
+    default = "010"
+}
+
+variable "vmname" {
+    type = string
+    default = "linux"
+}
+
+variable "admin_password" {
+    type    = string
+    default = "4wPcRFreYIVfCUYros9a"
+}
+
+variable "users" {
+    type = list
+    default = ["root", "azureuser"] 
+}
+
+variable "regions" {
+    type = map
+    default = {
+        "Global"        = "global"
+        "Central US"    = "centralus"
+        "East US"       = "eastus"
+        "East US 2"     = "eastus2"
+        "West Europe"   = "westeurope"
+        "North Europe"  = "northeurope"
+    }
+}
+
+variable "namingprefixes" {
+    type = map
+    default = {
+        "global"        = "glob"
+        "centralus"     = "cus1"
+        "eastus"        = "eus1"
+        "eastus2"       = "eus2"
+        "westeurope"    = "weu1"
+        "northeurope"   = "neu1"
+    }
+}
+
