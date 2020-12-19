6f3c97c90f7e99d5296ee1e39a504edfaf4ab041
Martin Georgiev <mggeorgiev@gmail.com>
2020-12-20 00:08:34 +0100

    add validation to storage account

--- storage/storage-account-module/variables.tf
+++ storage/storage-account-module/variables.tf
@@ -4,6 +4,27 @@ variable sto_location {
 
 variable sto_name {
     type    = string
+
+    validation {
+        condition     = length(var.sto_name) < 3 && length(var.sto_name) > 24
+        error_message = "The storage account name must be between 3 and 24 symbols."
+  }
+
+    validation {
+        condition     = lower(var.sto_name) != var.sto_name
+        error_message = "The storage account name lowercase characters"
+  }
+
+
+    validation {
+        condition     = regex([a-z0-9], var.sto_name) != var.sto_name
+        error_message = "The storage account name lowercase characters"
+  }
+
+      validation {
+        condition     = substr(var.sto_name, 0, 3) == "sto"
+        error_message = "The storage account name must start with sto"
+  }
 }
 
 variable tier {
