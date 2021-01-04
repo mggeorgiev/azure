939577a3f2025eaa12aa0dc6f288e2a5e28517ac
Martin Georgiev <mggeorgiev@gmail.com>
2021-01-04 16:12:06 +0100

    update listVMs

--- manage/listVms.sh
+++ manage/listVms.sh
@@ -1,1 +1,11 @@
-for subs in $()
+#!/bin/bash
+subscriptions=($(az account list --all --querry "[].name" -o tsv))
+ids=($(az account list --all --querry "[].id" -o tsv))
+
+for i in $(!subscriptions[@]);
+do
+    az account set --subscription ${ids[$i]}
+    echo "List of VMs in $(subscriptions[$i] subscription)"
+    az vm list -o table
+    echo ""
+done
