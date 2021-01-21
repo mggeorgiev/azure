721f2e6f93c9e4bb466601cffdd1df4e919386e2
Martin Georgiev <mggeorgiev@gmail.com>
2021-01-21 19:36:55 +0100

    site-to-site-vpn module

--- network/terraform/site-to-site-vpn/main.tf
+++ network/terraform/site-to-site-vpn/main.tf
@@ -1,5 +1,5 @@
 module "site_2_site_vpn_rsg" {
-    source              = "./../azure/terraform/rsg-module"
+    source              = "./../../../../azure/terraform/rsg-module"
     rg_name             = "site-to-site-vpn"
     rg_location         = "westeurope"
     environementtag     = "production"
@@ -7,7 +7,7 @@ module "site_2_site_vpn_rsg" {
 }
 
 module "site_2_site_vpn_vnet" {
-    source              = "./../azure/terraform/vnet-module"
+    source              = "./../../../../azure/terraform/vnet-module"
     resource_group      = module.site_2_site_vpn_rsg.rg_name
     address_space       = ["10.4.0.0/16"]
     address_prefixes    = ["10.4.0.0/24"]
@@ -30,4 +30,53 @@ resource "azurerm_subnet" "GatewaySubnet" {
 #       actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
 #     }
 #   }
+}
+
+resource "azurerm_public_ip" "site2sitevpnpip" {
+  name                = "site2sitevpnpip"
+  location            = module.site_2_site_vpn_rsg.rg_location
+  resource_group_name = module.site_2_site_vpn_rsg.rg_name
+
+  allocation_method = "Dynamic"
+}
+
+
+resource "azurerm_local_network_gateway" "onpremise" {
+  name                = "OnPremNet"
+  resource_group_name = module.site_2_site_vpn_rsg.rg_name
+  location            = module.site_2_site_vpn_rsg.rg_location
+  gateway_address     = var.onprempip
+  address_space       = ["192.168.88.0/24", "192.168.8.0/24"]
+}
+
+resource "azurerm_virtual_network_gateway" "site2sitevpngw" {
+  name                = "VNET1GW"
+  location            = module.site_2_site_vpn_rsg.rg_location
+  resource_group_name = module.site_2_site_vpn_rsg.rg_name
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+
+  active_active = false
+  enable_bgp    = false
+  sku           = "Basic"
+
+  ip_configuration {
+    name                          = "vnetGatewayConfig"
+    public_ip_address_id          = azurerm_public_ip.site2sitevpnpip.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.GatewaySubnet.id
   }
+}
+
+resource "azurerm_virtual_network_gateway_connection" "onpremise" {
+  name                = "onpremise"
+  location            = module.site_2_site_vpn_rsg.rg_location
+  resource_group_name = module.site_2_site_vpn_rsg.rg_name
+
+  type                       = "IPsec"
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.site2sitevpngw.id
+  local_network_gateway_id   = azurerm_local_network_gateway.onpremise.id
+
+  shared_key = var.shared_key
+}
---
+++ network/terraform/site-to-site-vpn/variable.tf
@@ -0,0 +1,7 @@
+variable "onprempip" {
+    type = string
+}
+
+variable "shared_key" {
+    type = string
+}
