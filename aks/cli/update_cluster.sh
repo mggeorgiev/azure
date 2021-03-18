e6fdf715dd128cfae5461bffbf792c1995a0d319
Martin Georgiev <mggeorgiev@gmail.com>
2021-03-18 09:42:35 +0000

    update cluster via cli

---
+++ aks/cli/update_cluster.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+az account list --output table
+read -p 'Please enter the desired subscription id: ' subscription
+az account set --subscription $subscription
+az account list --output table
+
+echo ''
+echo '---'
+echo ''
+
+az aks list --output table
+read -p 'Please enter the resource group name: ' resourceGroup
+read -p 'Please enter the aks name: ' aksName
+
+echo ''
+echo '---'
+echo ''
+
+az aks show --resource-group $resourceGroup --name $aksName --output table
+az aks get-upgrades --resource-group $resourceGroup --name $aksName
+
+echo ''
+echo '---'
+echo ''
+
+read -p 'To upgrade the cluster type yes: ' resourceGroup
+
+az aks upgrade \
+    --resource-group $resourceGroup \
+    --name $aksName \
+    --kubernetes-version KUBERNETES_VERSION
