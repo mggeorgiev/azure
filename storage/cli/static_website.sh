659b0ffebb9ef74d301d3f767b61d30f3315895f
Martin Georgiev <mggeorgiev@gmail.com>
2021-07-08 01:13:13 +0200

    update storage account

--- storage/cli/static_website.sh
+++ storage/cli/static_website.sh
@@ -14,21 +14,12 @@ az group create --name $resourceGroup --location $location
 
 az ad sp create-for-rbac --name $rolename --role contributor --scopes /subscriptions/${subscriptionId}/resourceGroups/${resourceGroup} --sdk-auth
 
-az storage account create --name $storageAccountname \ 
-                          --resource-group $resourceGroup \
-                          --access-tier Hot \
-                          --location $location \
-                          --sku Standard_LRS \
-                          --subscription $subscription
+az storage account create --name $storageAccountname --resource-group $resourceGroup --access-tier Hot --location $location --subscription $subscription
 
-az storage blob service-properties update \
-                                    --account-name $storageAccountname \
-                                    --static-website --index-document index.html \
-                                    --resource-group $resourceGroup \
-                                    --location $location
+az storage blob service-properties update --account-name $storageAccountname --static-website --index-document index.html 
 
 origin=$(az storage account show -g $resourceGroup -n $storageAccountname --query primaryEndpoints.web --output tsv)
 
-az cdn profile create -n $rolename --sku Standard_Microsoft
+az cdn profile create -n $rolename --resource-group $resourceGroup --sku Standard_Microsoft
 
-az cdn endpoint create -n ${rolename}cdnendpoint --profile-name $rolename --origin $origin --origin-host-header $origin --enable-compression
+az cdn endpoint create -n ${rolename}cdnendpoint --resource-group $resourceGroup --profile-name $rolename --origin $origin --origin-host-header $origin --enable-compression
