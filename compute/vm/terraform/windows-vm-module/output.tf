5891cb3e0fb208fa4d5e41f41148b47d546f895c
Martin Georgiev <mggeorgiev@gmail.com>
2021-09-09 19:00:51 +0200

    move terraform to separate repository

--- compute/vm/terraform/windows-vm-module/output.tf
+++
@@ -1,3 +0,0 @@
-output vmmsize {
-    value = azurerm_windows_virtual_machine.windowsvm.size
-}
--- resource_group/terraform/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "resourcegroup" {
-    name                = var.rg_name
-    location            = var.rg_location
-
-    tags = {
-        environment     = var.environementtag,
-        billing-code    = var.billing-code
-    }
-}
